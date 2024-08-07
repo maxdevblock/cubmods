55e0c5c6d51fd7545ac3f2db6377a2d4fb633fea
MaxDevBlock <maxdevblock@maxpierini.it>
2024-08-08 01:54:25 +0200

    numpydoc extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,12 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    #'sphinx.ext.napoleon',
+    "sphinx.ext.autosummary",  # Create neat summary tables for modules/classes/methods etc
+    #"sphinx.ext.intersphinx",  # Link to other project's documentation (see mapping below)
+    #"sphinx_autodoc_typehints",  # Automatically document param types (less noise in class signature)
+    'numpydoc' # Alternative to napoleon
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,7 +47,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [
+    'cubmods.cubsh2'
+]
 
 
 # -- Options for HTML output -------------------------------------------------
